3aba6689998aebc13ff6ab30a8790a8e4731b017
dszwarc <dszwarc93@gmail.com>
2024-10-11 12:00:04 -0400

    added style to job model

--- culet/models.py
+++ culet/models.py
@@ -2,6 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
+
+    RING = "RG"
+    BRACELET = "BR"
+    NECKLACE = "NK"
+    
+
     name = models.CharField(max_length=80)
     customer = models.CharField(max_length=80)
     job_num = models.IntegerField
+    style = models.CharField(
+        max_length = 2,
+        choices = {
+            RING : "Ring",
+            BRACELET : "Bracelet",
+            NECKLACE : "Necklace",
+            },
+        default = RING
+        )
+        
