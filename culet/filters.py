658955fdaf72f0a4aab8d27a3f96e6f4a9443e41
dszwarc <dszwarc93@gmail.com>
2025-03-26 22:14:07 -0400

    cleaned up urls and removed unneeded filters

--- culet/filters.py
+++ culet/filters.py
@@ -1,14 +1,15 @@
 import django_filters
-from django_filters import DateFilter, CharFilter
+from django_filters import DateFilter, CharFilter, DateTimeFilter
 from .models import *
 
 class JobFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name="created", lookup_expr='gte')
-    end_date = DateFilter(field_name="created", lookup_expr='lte')
-    
-    customer = CharFilter(field_name='customer', lookup_expr='icontains')
+    start_date = DateFilter(label='Created after:',field_name="created", lookup_expr='gte')
+    end_date = DateFilter(label='Created before:', field_name="created", lookup_expr='lte')
+    due_date = DateTimeFilter(label='Due Before:',field_name='due', lookup_expr='lte')
+
+    customer = CharFilter(label='Customer',field_name='customer', lookup_expr='icontains')
     
     class Meta:
         model = Job
         fields = '__all__'
-        exclude = ['date','name']
+        exclude = ['date','name','created','last_updated','due']
