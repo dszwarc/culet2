487fd03667d374ccada77fb6b87ea142680b0387
dszwarc <dszwarc93@gmail.com>
2025-06-02 12:45:12 -0400

    added requirements.txt

--- celjewelry/settings.py
+++ celjewelry/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env=environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%bwp%-1qh3j3&de=g4=9p1$kde8l^72dg-wv9b8b=z!^4=!)bg'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ["localhost"]
 
@@ -72,10 +71,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'celjewelry.wsgi.application'
 
-import environ
 
-env=environ.Env()
-environ.Env.read_env()
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env("DEBUG") == "True"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
